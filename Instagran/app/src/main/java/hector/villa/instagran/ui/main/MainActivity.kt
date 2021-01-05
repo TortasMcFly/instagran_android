75e35b5821f78ee5ad6653cbabe5705e677b4df2
Hector Luevano <hecth77@gmail.com>
2021-01-05 16:45:45 -0600

    commit inicial

---
+++ Instagran/app/src/main/java/hector/villa/instagran/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package hector.villa.instagran.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import hector.villa.instagran.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
