4e1b701a1fcb76b84def32c84113bcde97b1e7d4
Arte <arte.solo.arte@hotmail.com>
2021-01-08 18:14:07 -0600

    Implementación de Navegación
    Implementación de Fragmentos para la barra de navegación

--- Instagran/app/src/main/java/hector/villa/instagran/ui/main/MainActivity.kt
+++ Instagran/app/src/main/java/hector/villa/instagran/ui/main/MainActivity.kt
@@ -2,11 +2,19 @@ package hector.villa.instagran.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import hector.villa.instagran.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController: NavController = Navigation.findNavController(this, R.id.host_fragment)
+        val buttomNavigation: BottomNavigationView = findViewById<BottomNavigationView>(R.id.buttom_navigation)
+        NavigationUI.setupWithNavController(buttomNavigation, navController)
     }
 }
