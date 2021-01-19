aba7cedfa55f2c71b89f7956ea58e97ecc002c4f
Alejandro García Olivo <ago99@live.com.mx>
2021-01-19 11:26:49 -0600

    commit

---
+++ Instagran/app/src/main/java/hector/villa/instagran/ui/inicio/HistoriaAdapter.kt
@@ -0,0 +1,31 @@
+package hector.villa.instagran.ui.inicio
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import hector.villa.instagran.R
+
+class HistoriaAdapter( val historias: ArrayList<String>, val context: Context) : RecyclerView.Adapter<HistoriaAdapter.HistoriaViewHolder>() {
+
+    class HistoriaViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val tvUsername = view.findViewById<TextView>(R.id.tvUsername)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HistoriaViewHolder {
+        return HistoriaViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.item_historia, parent)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return historias.size
+    }
+
+    override fun onBindViewHolder(holder: HistoriaViewHolder, position: Int) {
+        holder.tvUsername.text = historias[position]
+    }
+
+}
