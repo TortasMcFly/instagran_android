7ebe817de33cfeb93f5b08ba322e79906b629778
TortasMcFly <hecth77@gmail.com>
2021-02-19 17:24:14 -0600

    Swipe Right and Left

--- Instagran/app/src/main/java/hector/villa/instagran/ui/detalle_historia/DetalleHistoriaActivity.kt
+++ Instagran/app/src/main/java/hector/villa/instagran/ui/detalle_historia/DetalleHistoriaActivity.kt
@@ -3,6 +3,7 @@ package hector.villa.instagran.ui.detalle_historia
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 import android.widget.ImageView
@@ -11,12 +12,16 @@ import com.bumptech.glide.Glide
 import com.teresaholfeld.stories.StoriesProgressView
 import hector.villa.instagran.R
 import hector.villa.instagran.domain.Historia
+import java.lang.Math.abs
 
-class DetalleHistoriaActivity : AppCompatActivity(), StoriesProgressView.StoriesListener {
-
+class DetalleHistoriaActivity : AppCompatActivity(),
+        StoriesProgressView.StoriesListener,
+        GestureDetector.OnGestureListener
+{
     private lateinit var historias: ArrayList<Historia>
     private lateinit var historia: Historia
     private var startUserPosition = 0
+    private var storyPosition = 0
     private lateinit var btnCloseStory: View
     private lateinit var imageViewHistoria: ImageView
     private lateinit var storyImage: ImageView
@@ -24,10 +29,12 @@ class DetalleHistoriaActivity : AppCompatActivity(), StoriesProgressView.Stories
     private lateinit var profileArea: View
     private lateinit var areaIzquierda: View
     private lateinit var areaDerecha: View
-    private lateinit var cardview: View
+    private lateinit var areaCentro: View
     private lateinit var storiesProgressView: StoriesProgressView
 
-    private var position = 0
+    lateinit var gestureDetector: GestureDetector
+    private val swipeThreshold = 100
+    private val swipeVelocityThreshold = 100
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,6 +44,7 @@ class DetalleHistoriaActivity : AppCompatActivity(), StoriesProgressView.Stories
         startUserPosition = intent.getIntExtra("start_position", 0)
         setupUi()
         changeUserStories()
+        gestureDetector = GestureDetector(this, this)
     }
 
     private fun setupUi() {
@@ -53,7 +61,7 @@ class DetalleHistoriaActivity : AppCompatActivity(), StoriesProgressView.Stories
         areaIzquierda = findViewById(R.id.areaIzquierda)
         areaDerecha = findViewById(R.id.areaDerecha)
         storiesProgressView = findViewById(R.id.stories)
-        cardview = findViewById(R.id.cardview)
+        areaCentro = findViewById(R.id.areaCentro)
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -63,7 +71,7 @@ class DetalleHistoriaActivity : AppCompatActivity(), StoriesProgressView.Stories
         }
 
         areaIzquierda.setOnClickListener {
-            if(position > 0) storiesProgressView.reverse()
+            if(storyPosition > 0) storiesProgressView.reverse()
             else if(startUserPosition != 0) {
                 startUserPosition--
                 changeUserStories()
@@ -74,7 +82,7 @@ class DetalleHistoriaActivity : AppCompatActivity(), StoriesProgressView.Stories
             storiesProgressView.skip()
         }
 
-        cardview.setOnTouchListener { v, event ->
+        areaCentro.setOnTouchListener { v, event ->
             if(event.action == MotionEvent.ACTION_DOWN) storiesProgressView.pause()
             else if(event.action == MotionEvent.ACTION_UP) storiesProgressView.resume()
 
@@ -88,7 +96,7 @@ class DetalleHistoriaActivity : AppCompatActivity(), StoriesProgressView.Stories
 
     private fun changeUserStories() {
         historia = historias[startUserPosition]
-        position = 0
+        storyPosition = 0
         setupStoriesProgressView()
         tvUsername.text = historia.username
 
@@ -102,8 +110,8 @@ class DetalleHistoriaActivity : AppCompatActivity(), StoriesProgressView.Stories
         storiesProgressView.setStoryDuration(3000L)
         storiesProgressView.setStoriesListener(this)
 
-        storiesProgressView.startStories(position)
-        setStoryImage(position)
+        storiesProgressView.startStories(storyPosition)
+        setStoryImage(storyPosition)
     }
 
     private fun setStoryImage(pos: Int) {
@@ -119,17 +127,64 @@ class DetalleHistoriaActivity : AppCompatActivity(), StoriesProgressView.Stories
     }
 
     override fun onNext() {
-        position++
-        setStoryImage(position)
+        storyPosition++
+        setStoryImage(storyPosition)
     }
 
     override fun onPrev() {
-        position--
-        setStoryImage(position)
+        storyPosition--
+        setStoryImage(storyPosition)
     }
 
     override fun onDestroy() {
         storiesProgressView.destroy()
         super.onDestroy()
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        return if (gestureDetector.onTouchEvent(event)) {
+            true
+        }
+        else {
+            super.onTouchEvent(event)
+        }
+    }
+
+    override fun onDown(e: MotionEvent?): Boolean = false
+
+    override fun onShowPress(e: MotionEvent?) { }
+
+    override fun onSingleTapUp(e: MotionEvent?): Boolean = false
+
+    override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean = false
+
+    override fun onLongPress(e: MotionEvent?) { }
+
+    override fun onFling(e1: MotionEvent, e2: MotionEvent, velocityX: Float, velocityY: Float): Boolean {
+        try {
+            val diffY = e2.y - e1.y
+            val diffX = e2.x - e1.x
+            if (abs(diffX) > abs(diffY)) {
+                if (abs(diffX) > swipeThreshold && abs(velocityX) > swipeVelocityThreshold) {
+                    if (diffX > 0) {
+                        //Toast.makeText(applicationContext, "Left to Right swipe gesture", Toast.LENGTH_SHORT).show()
+                        if(startUserPosition > 0) {
+                            startUserPosition--
+                            changeUserStories()
+                        }
+                    }
+                    else {
+                        //Toast.makeText(applicationContext, "Right to Left swipe gesture", Toast.LENGTH_SHORT).show()
+                        startUserPosition++
+                        if(startUserPosition >= historias.size) finish()
+                        else changeUserStories()
+                    }
+                }
+            }
+        }
+        catch (exception: Exception) {
+            exception.printStackTrace()
+        }
+        return true
     }
 }
